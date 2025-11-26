a15179f274a984a20e14d66076ff5c651d8f7641
Guerra24 <lili@guerra24.net>
2025-11-25 20:00:00 -0600

    Use fixed commit

--- tools/install.pl
+++ tools/install.pl
@@ -160,7 +160,7 @@ if ( $back || $full ) {
         say("Installing dependencies for windows systems... (This will do nothing if the package is there already)");
 
         install_package( "Win32::Process", $cpanopt );
-        install_package( "Win32::FileSystemHelper", "https://github.com/Guerra24/Win32-FileSystemHelper/archive/e0cc6443f6c88bf2281c99dc7ef32bca7628e707.zip " . $cpanopt );
+        install_package( "Win32::FileSystemHelper", "https://github.com/Guerra24/Win32-FileSystemHelper/archive/308b92c958bb4931dfd704cc5025f93e28ef0c8a.zip " . $cpanopt );
         install_package( "File::ChangeNotify::Watcher::Win32", "https://github.com/Guerra24/File-ChangeNotify-Watcher-Win32/archive/7cb4e60823569cca8e7652d19b1ba5b5cac00a16.zip " .$cpanopt );
     }
 
